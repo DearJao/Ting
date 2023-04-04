32a961ee1ee79dce539895f78a844e29fa119160
DearJao <jvvr2013@gmail.com>
2023-04-04 16:17:13 -0300

    função txt_importer capaz de ler arquivos .txt criada

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação bem aqui ó"""
+    if not os.path.exists(path_file):
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    _, file_ext = os.path.splitext(path_file)
+    if file_ext.lower() != ".txt":
+        return print("Formato inválido", file=sys.stderr)
+
+    with open(path_file, "r") as file:
+        return file.read().split("\n")
