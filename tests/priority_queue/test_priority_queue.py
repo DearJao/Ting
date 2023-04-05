678d241793ff7f11df3e558967f1c5e2b57be91c
DearJao <jvvr2013@gmail.com>
2023-04-05 17:23:34 -0300

    teste da classe PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,37 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+
+    seventh_item_nopreferred = {'qtd_linhas': 7}
+    sixth_item_nopreferred = {'qtd_linhas': 6}
+    first_item_preferred = {'qtd_linhas': 1}
+    second_item_preferred = {'qtd_linhas': 2}
+    third_item_preferred = {'qtd_linhas': 3}
+
+    pq.enqueue(sixth_item_nopreferred)
+    pq.enqueue(first_item_preferred)
+    pq.enqueue(second_item_preferred)
+    pq.enqueue(third_item_preferred)
+    pq.enqueue(seventh_item_nopreferred)
+    assert len(pq) == 5
+
+    assert pq.search(2) == third_item_preferred
+    assert pq.search(3) == sixth_item_nopreferred
+    with pytest.raises(IndexError):
+        pq.search(10)
+
+    assert pq.dequeue() == first_item_preferred
+    assert pq.dequeue() == second_item_preferred
+    assert pq.dequeue() == third_item_preferred
+    assert pq.dequeue() == sixth_item_nopreferred
+    assert pq.dequeue() == seventh_item_nopreferred
+
+    assert len(pq) == 0
+
+    pq.enqueue(seventh_item_nopreferred)
+    pq.enqueue(sixth_item_nopreferred)
+
+    assert len(pq) == 2
